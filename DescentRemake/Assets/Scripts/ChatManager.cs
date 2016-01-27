950325dec3d6f72931b66685dd6f5085fa50f164
unknown <Inno@TYOD-1342-E01L.kk.lan>
2016-01-27 14:17:20 +0200

    ChatApi Canvas almost done

--- DescentRemake/Assets/Scripts/ChatManager.cs
+++ DescentRemake/Assets/Scripts/ChatManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using ExitGames.Client.Photon.Chat;
+using UnityEngine.UI;
 
 public class ChatManager : MonoBehaviour, IChatClientListener
 {
@@ -21,19 +22,33 @@ public class ChatManager : MonoBehaviour, IChatClientListener
 
     private bool _connected;
 
+    private Text ChatText;
+    private InputField ChatInput;
+
+    ExitGames.Client.Photon.Chat.AuthenticationValues authValues = new ExitGames.Client.Photon.Chat.AuthenticationValues();
+
+
+
+
+
+
 
     public void DebugReturn(ExitGames.Client.Photon.DebugLevel level, string message)
     {
-
+        Debug.Log(message);
     }
 
     // Use this for initialization
     void Start()
     {
+        ChatText = GameObject.Find("ChatText").GetComponent<Text>();
+        ChatInput = GameObject.Find("ChatInput").GetComponent<InputField>();
         DontDestroyOnLoad(gameObject);
         Application.runInBackground = true;
 
         _chat = new ChatClient(this);
+        
+
     }
 
     // Update is called once per frame
@@ -41,33 +56,58 @@ public class ChatManager : MonoBehaviour, IChatClientListener
     {
         if (_chat != null)
             _chat.Service();
+
+        _chat.Service();
     }
 
     void OnGUI()
     {
+
         if (!_connected)
         {
-            username = GUI.TextField(new Rect(10, 10, 200, 20), username);
+            Connect();
+
+           /* username = GUI.TextField(new Rect(10, 10, 200, 20), username);
             if (GUI.Button(new Rect(10, 35, 80, 20), "Enter"))
             {
+
                 if (!string.IsNullOrEmpty(username) && username.Length > 0)
+                {
                     Connect();
-            }
+                    Debug.Log("Connecting");
+
+                }
+            }*/
         }
         else
         {
-            GUI.TextArea(new Rect(10, 10, 200, 200), _chatText);
-            _input = GUI.TextField(new Rect(10, 215, 100, 20), _input);
-            if (GUI.Button(new Rect(115, 215, 80, 20), "Send"))
+
+            ChatText.text = _chatText;
+            //GUI.TextArea(new Rect(10, 10, 200, 200), _chatText);
+            _input = ChatInput.text;
+            //_input = GUI.TextField(new Rect(10, 215, 100, 20), _input);
+            
+            if (Input.GetKeyDown(KeyCode.Return))
             {
-                if (!string.IsNullOrEmpty(_input) && _input.Length > 0)
+                if (ChatInput.enabled)
                 {
-                    SendMessage(_input);
-                    _input = "";
+                    if (!string.IsNullOrEmpty(_input) && _input.Length > 0)
+                    {
+                        SendMessage(_input);
+                    }
+                    ChatInput.text = "";
+                    ChatInput.enabled = false;
+                    ChatInput.GetComponent<CanvasGroup>().alpha = 0;
+                }
+                else
+                {
+                    ChatInput.GetComponent<CanvasGroup>().alpha = 1;
+                    ChatInput.enabled = true;
                 }
             }
-        }
 
+
+        }
     }
 
     void OnApplicationQuit()
@@ -78,7 +118,20 @@ public class ChatManager : MonoBehaviour, IChatClientListener
 
     private void Connect()
     {
-        _chat.Connect(AppId, AppVersion, null);
+        username = PhotonNetwork.player.name;
+        authValues.UserId = username;
+        authValues.AuthType = ExitGames.Client.Photon.Chat.CustomAuthenticationType.None;
+
+
+        if (!_chat.Connect(AppId, AppVersion, this.authValues))
+        {
+            Debug.Log("Error connecting server");
+            _chat.DebugOut = ExitGames.Client.Photon.DebugLevel.ALL;
+        }
+        else
+            _connected = true;
+        
+        Debug.Log("Connected");
     }
 
     private void SendMessage(string message)
@@ -114,6 +167,7 @@ public class ChatManager : MonoBehaviour, IChatClientListener
 
     public void OnDisconnected()
     {
+
         _connected = false;
     }
 
@@ -123,7 +177,7 @@ public class ChatManager : MonoBehaviour, IChatClientListener
 
         _chat.Subscribe(new[] { GeneralChannel }, -1);
 
-        //_chat.SetFriendList(new[] { "admin" });
+       // _chat.SetFriendList(new[] { "admin" });
         _chat.SetOnlineStatus(ChatUserStatus.Online);
     }
 
@@ -152,13 +206,16 @@ public class ChatManager : MonoBehaviour, IChatClientListener
     {
         foreach (var channel in channels)
         {
-            _chat.PublishMessage(channel, "Hello.");
+            _chat.PublishMessage(channel, "Connected.");
         }
     }
 
     public void OnUnsubscribed(string[] channels)
     {
-
+        foreach (var channel in channels)
+        {
+            _chat.PublishMessage(channel, "Disconnected.");
+        }
     }
 
     public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
